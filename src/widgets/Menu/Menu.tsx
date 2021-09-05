b645bb1f5abe42041ed002dba7aa7b13f207eb9c
sam-thewise <caliginosity.insanity@gmail.com>
2021-09-05 15:03:22 +1200

    layer changes

--- src/widgets/Menu/Menu.tsx
+++ src/widgets/Menu/Menu.tsx
@@ -34,7 +34,8 @@ const StyledNav = styled.nav<{ showMenu: boolean }>`
   padding-right: 16px;
   width: 100%;
   height:112px;
-  background-color: rgba(97,105,182,0.9);
+  background-color: rgba(97,105,182,0.65);
+  backdrop-filter: blur(8px);
   z-index: 20;
   border-bottom: 2px solid #efcd52;
   transform: translate3d(0, 0, 0);

ec87ab249e8e2358fc42e049ae358a4005e7a8d8
sam-thewise <caliginosity.insanity@gmail.com>
2021-09-24 15:13:43 +1200

    Update Menu.tsx

--- src/widgets/Menu/Menu.tsx
+++ src/widgets/Menu/Menu.tsx
@@ -254,6 +254,9 @@ const Menu: React.FC<NavProps> = ({
               <InfoBoxes>
                   <Link href={paladinAuditLink} target="_blank"><img className="navImg" src="https://paladinsec.co/pld/assets/audited-by-paladin-standard.svg" alt="paladin audit"/></Link>
               </InfoBoxes>
+              <InfoBoxes>
+                  <Link href="https://tinydragon.games/" target="_blank"><img className="navImg" src="https://tinydragon.games/img/logo.png" alt="Tiny Dragons!"/></Link>
+              </InfoBoxes>
           </div>
             
        </DLInfoContainer>
